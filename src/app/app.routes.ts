bd4026e9675ab8de6af4636f20d7e844d3525d3c
mithun15 <mithun.kumarr6@gmail.com>
2025-08-23 18:35:12 +0530

    Revert "created signin and signup"
    This reverts commit 974619937b065bbda0b2078cb943dd295fccbfdd.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './auth/dashboard/dashboard';
 
-export const routes: Routes = [
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.routes').then((m) => m.routes),
-  },
-  {
-    path: 'dashbaord',
-    component: Dashboard,
-  },
-];
+export const routes: Routes = [];
--- src/app/auth/signup-detail.ts
+++
@@ -1,7 +0,0 @@
-export interface SignUpDetails {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    role?: 'admin' | 'user';
-}
